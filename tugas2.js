7a1baf5157b023e6cbc5d42f20e6bff84d76ca57
syafacode <syafacode.01@gmail.com>
2020-04-26 00:45:39 +0700

    tipe data

---
+++ tugas2.js
@@ -0,0 +1,32 @@
+//array
+var user = ["Pak Kades", "naik delman", "istimewa", 20, null, false, true];
+console.log(
+    `${user[0]} sedang ${user[1]} ${user[2]} sejauh ${user[3]}km`
+);
+
+//undifined
+var notnot;
+console.log(notnot);
+
+//null
+var nothing = null;
+console.log(nothing);
+
+//string
+var kata = "benar";
+kata = "salah";
+console.log(kata);
+
+//number
+var angka = 10;
+angka = 3.14;
+console.log(angka);
+
+//bolean
+var suhu = true;
+angka = 38;
+if (suhu) {
+    console.log(suhu + " positif corona, suhu anda = " + angka);
+} else {
+    console.log(suhu + " corona, suhu anda =" + angka);
+}
